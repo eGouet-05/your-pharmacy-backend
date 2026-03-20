1dea5348365d8a84b8348445d88a5cb05bf98a74
GOUET GNANGNAN GRACE ESTHER <128064996+eGouet-05@users.noreply.github.com>
2026-03-20 23:08:18 +0000

    Add SSL options to Sequelize configuration
    Enable SSL for PostgreSQL connection in production.

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,8 +3,12 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
-  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // important pour Render
+    },
+  },
 });
 
 module.exports = sequelize;
-
